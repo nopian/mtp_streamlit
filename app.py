55ea7e543489edf3d067cfcdb9dfabf54cfd2676
Ian <nopian@users.noreply.github.com>
2024-02-04 11:01:30 -0500

    changes

--- app.py
+++ app.py
@@ -27,45 +27,56 @@ def display_map(location_data:pd.DataFrame):
     map_data = st_folium(m, width=1500, height=1200, returned_objects=["last_object_clicked"])
 
 st.set_page_config(
-    page_title="Mount Pleasant Projects",
+    page_title="MT P - Projects",
     layout="wide"
 )
 
-st.header('Mount Pleasant Projects')
+st.header('MT P - Projects')
 
 #Grab CSVs
 town_projects = pd.read_csv("https://nopian.github.io/govquery/projects.csv",
-                            usecols=['Name', 'Group', 'Agenda', 'latitude', 'longitude'])
+                            usecols=['Name', 'Group', 'Agenda', 'latitude', 'longitude', 'date'])
 
 town_projects = town_projects[town_projects["Group"].str.contains("Old Village Historic District") == False]
 town_projects = town_projects[town_projects["Group"].str.contains("Board of Zoning Appeals") == False]
 town_projects = town_projects[town_projects["Group"].str.contains("Planning Commission") == False]
 
+town_projects['date'] = pd.to_datetime(town_projects['date'],format= '%Y-%m-%d')
 town_projects = town_projects.drop(columns=['Group'])
-#Use date
-town_projects.columns = ['Name', 'Desc', 'latitude', 'longitude']
 
+town_projects.columns = ['Name', 'Desc', 'latitude', 'longitude', 'date']
 
+####
+####
 mpw_projects = pd.read_csv("https://nopian.github.io/govquery/mpw_projects.csv",
-                           usecols=['PROJECT NAME', 'WebsiteDesc', 'latitude', 'longitude'])
-#Use date
-mpw_projects.columns = ['Name', 'Desc', 'latitude', 'longitude']
+                           usecols=['PROJECT NAME', 'WebsiteDesc', 'latitude', 'longitude', 'date'])
 
+mpw_projects['date'] = pd.to_datetime(mpw_projects['date'],format= '%Y-%m-%d')
+mpw_projects.columns = ['Name', 'Desc', 'latitude', 'longitude', 'date']
 
+####
+####
 dhec_permits = pd.read_csv("https://nopian.github.io/govquery/dhec_permits.csv",
-                           usecols=['siteName', 'siteProfileUrl', 'latitude', 'longitude'])
-#Use date
-dhec_permits.columns = ['latitude', 'longitude', 'Name', 'Desc' ]
+                           usecols=['siteName', 'siteProfileUrl', 'latitude', 'longitude', 'date'])
 
+dhec_permits['date'] = pd.to_datetime(dhec_permits['date'],format= '%Y-%m-%d')
+dhec_permits.columns = ['latitude', 'longitude', 'Name', 'Desc', 'date' ]
+
+####
+####
 town_stormwater = pd.read_csv("https://nopian.github.io/govquery/stormwater.csv",
-                              usecols=['ProjectName', 'URL', 'latitude', 'longitude'])
-#Use OpenDate
-town_stormwater.columns = ['Name', 'Desc', 'latitude', 'longitude']
+                              usecols=['ProjectName', 'URL', 'latitude', 'longitude', 'date'])
 
+town_stormwater['date'] = pd.to_datetime(town_stormwater['date'],format= '%Y-%m-%d')
+town_stormwater.columns = ['Name', 'Desc', 'latitude', 'longitude', 'date']
+
+####
+####
 chs_projects = pd.read_csv("https://nopian.github.io/govquery/chs_newconstruction.csv",
-                           usecols=['PERMIT_ADDRESS_LINE1', 'DESCRIPTION', 'latitude', 'longitude'])
-#Use date
-chs_projects.columns = ['Desc', 'Name', 'latitude', 'longitude']
+                           usecols=['PERMIT_ADDRESS_LINE1', 'DESCRIPTION', 'latitude', 'longitude', 'date'])
+
+chs_projects['date'] = pd.to_datetime(chs_projects['date'],format= '%Y-%m-%d')
+chs_projects.columns = ['Desc', 'Name', 'latitude', 'longitude', 'date']
 
 
 #Choose Data
@@ -83,6 +94,9 @@ m2 = all_frames["Desc"].str.contains(text_search, na=False, case=False)
 df_search = all_frames[m1 | m2]
 if text_search:
     st.write(df_search)
+
+st.write("Last 7 days")
+st.write(all_frames[all_frames.date > pd.Timestamp.now() - pd.to_timedelta("7day")])
     
 if option == "All":
     #Display Map
@@ -112,3 +126,4 @@ if option == "CHS New Construction":
     
     #Display Map
     display_map(chs_projects)
+    
