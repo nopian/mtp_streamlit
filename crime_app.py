fec952a5f8677bf3bbf0e65d2513163cd694c14c
Ian <nopian@users.noreply.github.com>
2024-06-16 18:13:37 -0400

    update crime app

--- crime_app.py
+++ crime_app.py
@@ -1,47 +1,20 @@
-import requests
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-from datetime import datetime
 
 # Set the layout to wide
 st.set_page_config(layout="wide")
 
-# Load the JSON data from a URL
+# Load the CSV data
 @st.cache_data
-def load_data():
-    url = 'https://nopian.github.io/crime/combined_crime_data.json'
-    response = requests.get(url)
-    data = response.json()
-    return data
+def load_data(file_path):
+    df = pd.read_csv(file_path)
+    df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y %I:%M:%S %p')
+    return df
 
-data = load_data()
-
-# Create a pandas DataFrame from the JSON data
-df = pd.DataFrame(data)
-
-# Convert the 'InfoBoxString' to extract 'Case #', 'Date', and 'Location'
-def extract_info(box_string, label):
-    prefix = f"<bold>{label}</bold>"
-    start = box_string.find(prefix) + len(prefix)
-    end = box_string.find('<br>', start)
-    return box_string[start:end].strip()
-
-df['Case #'] = df['InfoBoxString'].apply(lambda x: extract_info(x, 'Case # '))
-df['Date'] = df['InfoBoxString'].apply(lambda x: extract_info(x, 'Date:'))
-df['Location'] = df['InfoBoxString'].apply(lambda x: extract_info(x, 'Location:'))
-
-# Correct the misspelling of "Longitute" to "Longitude"
-df.rename(columns={"Longitute": "Longitude"}, inplace=True)
-
-# Convert the 'Date' column to datetime
-df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y %I:%M:%S %p')
-
-# Set the 'Date' column as the index
-df.set_index('Date', inplace=True)
-
-# Resample the data to monthly intervals and count the number of occurrences in each 'Group'
-group_counts = df.groupby([pd.Grouper(freq='M'), 'Group']).size().unstack(fill_value=0)
+# Load the combined data
+data_url = 'https://nopian.github.io/crime/combined_crime_data.csv'
+df = load_data(data_url)
 
 # Streamlit app
 st.title("Crime Analysis Overview")
@@ -51,6 +24,7 @@ This app provides an interactive overview of crime data with various visualizati
 
 # Monthly Overview of Crime Counts
 st.subheader("Monthly Overview of Crime Counts")
+group_counts = df.groupby([pd.Grouper(key='Date', freq='M'), 'Group']).size().unstack(fill_value=0)
 group_counts = group_counts.reset_index().melt(id_vars='Date', var_name='Group', value_name='Count')
 
 fig = px.bar(group_counts, x='Date', y='Count', color='Group', barmode='stack', 
@@ -66,12 +40,9 @@ fig_trend = px.line(group_counts, x='Date', y='Count', color='Group',
                     labels={'Date': 'Month', 'Count': 'Crime Count'}, title='Crime Trends by Group Over Time')
 st.plotly_chart(fig_trend, use_container_width=True)
 
-# Reset index to include 'Date' column for the map plot
-df = df.reset_index()
-
 # Map of Crime Locations
 st.subheader("Map of Crime Locations")
-fig_map = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", color="Group", hover_name="Title", hover_data=["Case #", "Location", "Date"],
+fig_map = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", color="Group", hover_name="Group", hover_data=["CaseNumber", "Location", "Date"],
                             color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10)
 fig_map.update_layout(mapbox_style="open-street-map")
 fig_map.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
@@ -81,7 +52,7 @@ st.plotly_chart(fig_map, use_container_width=True)
 # Crime Heatmap
 st.subheader("Crime Heatmap")
 fig_heatmap = px.density_mapbox(df, lat='Latitude', lon='Longitude', z=None, radius=10, 
-                                hover_name="Title", hover_data=["Case #", "Location", "Date"], 
+                                hover_name="Group", hover_data=["CaseNumber", "Location", "Date"], 
                                 mapbox_style="open-street-map", zoom=10)
 fig_heatmap.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
@@ -129,9 +100,13 @@ crime_summary.columns = ['Crime Type', 'Count']
 st.write(crime_summary)
 
 # Data Editor
-st.subheader("View Data")
-edited_df = st.data_editor(df,hide_index=True, disabled=True, width=1800)
+st.subheader("Edit Data")
+edited_df = st.data_editor(df)
+st.write("You can edit the data in the table below:")
 
+# Show data as table
+st.subheader("Data Table")
+st.dataframe(group_counts)
 
 # Option to download data
 st.download_button(
